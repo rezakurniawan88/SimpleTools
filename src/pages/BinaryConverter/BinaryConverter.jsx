5e6ef191e13b9f9365bda3e5dec72fdb647b89f8
rezakurniawan88 <rezakurniawan.rk10@gmail.com>
2022-08-24 23:43:33 +0700

    add new component number

--- src/pages/BinaryConverter/BinaryConverter.jsx
+++ src/pages/BinaryConverter/BinaryConverter.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import NumberInput from "../../components/Input/NumberInput";
+import ImutableNumberInput from "../../components/Input/ImutableNumberInput";
 
 function BinaryConverter() {
     const [binaryValue, setBinaryValue] = useState(0);
@@ -15,25 +17,11 @@ function BinaryConverter() {
     return (
         <div className="binary-converter pb-7">
             <h1 className="text-3xl font-bold text-center my-6">Binary Converter</h1>
-            <div className="converter-dec-to-bin w-4/5 md:w-2/3 m-auto my-5">
-                <h2 className="font-semibold pb-3">Masukkan angka binary disini</h2>
-                <div className="decimal-to-binary flex gap-8">
-                    <input type="number" onChange={(e) => setBinaryValue(e.target.value)} name="decimal" id="decimal" className="min-w-full p-4 border border-blue-300 rounded-lg" placeholder="Masukkan angka ..." />
-                </div>
-            </div>
+            <NumberInput title="Masukkan angka binary disini" name="binary" id="binary" onChange={(e) => setBinaryValue(e.target.value)} />
             <div className="converter-dec-to-bin w-4/5 md:w-2/3 m-auto flex flex-wrap gap-5">
-                <div className="w-full md:w-[48%]">
-                    <h2 className="font-semibold pb-3">Octal</h2>
-                    <input type="number" name="binaryResult" id="binaryResult" className="w-full p-4 bg-slate-100 border border-gray-500 rounded-lg focus:outline-none" value={octalValue} readOnly />
-                </div>
-                <div className="w-full md:w-[48%]">
-                    <h2 className="font-semibold pb-3">Decimal</h2>
-                    <input type="number" name="octalResult" id="octalResult" className="w-full p-4 bg-slate-100 border border-gray-500 rounded-lg focus:outline-none" value={decimalValue} readOnly />
-                </div>
-                <div className="w-full md:w-[48%]">
-                    <h2 className="font-semibold pb-3">Hexadecimal</h2>
-                    <input type="number" name="hexaResult" id="hexaResult" className="w-full p-4 bg-slate-100 border border-gray-500 rounded-lg focus:outline-none" value={hexaValue} readOnly />
-                </div>
+                <ImutableNumberInput title="Octal" name="octalResult" id="octalResult" value={octalValue} />
+                <ImutableNumberInput title="Decimal" name="decimalResult" id="decimalResult" value={decimalValue} />
+                <ImutableNumberInput title="Hexadecimal" name="hexaResult" id="hexaResult" value={hexaValue} />
             </div>
         </div>
     )
--- src/pages/DecimalConverter/DecimalConverter.jsx
+++ src/pages/DecimalConverter/DecimalConverter.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import NumberInput from "../../components/Input/NumberInput";
+import ImutableNumberInput from "../../components/Input/ImutableNumberInput";
 
 function DecimalConverter() {
     const [decimalValue, setDecimalValue] = useState(0);
@@ -15,25 +17,11 @@ function DecimalConverter() {
     return (
         <div className="decimal-converter pb-7">
             <h1 className="text-3xl font-bold text-center my-6">Decimal Converter</h1>
-            <div className="converter-dec-to-bin w-4/5 md:w-2/3 m-auto my-5">
-                <h2 className="font-semibold pb-3">Masukkan angka decimal disini</h2>
-                <div className="decimal-to-binary flex gap-8">
-                    <input type="number" onChange={(e) => setDecimalValue(e.target.value)} name="decimal" id="decimal" className="min-w-full p-4 border border-blue-300 rounded-lg" placeholder="Masukkan angka ..." />
-                </div>
-            </div>
+            <NumberInput title="Masukkan angka decimal disini" name="decimal" id="decimal" onChange={(e) => setDecimalValue(e.target.value)} />
             <div className="converter-dec-to-bin w-4/5 md:w-2/3 m-auto flex flex-wrap gap-5">
-                <div className="w-full md:w-[48%]">
-                    <h2 className="font-semibold pb-3">Binary</h2>
-                    <input type="number" name="binaryResult" id="binaryResult" className="w-full p-4 bg-slate-100 border border-gray-500 rounded-lg focus:outline-none" value={binaryValue} readOnly />
-                </div>
-                <div className="w-full md:w-[48%]">
-                    <h2 className="font-semibold pb-3">Octal</h2>
-                    <input type="number" name="octalResult" id="octalResult" className="w-full p-4 bg-slate-100 border border-gray-500 rounded-lg focus:outline-none" value={octalValue} readOnly />
-                </div>
-                <div className="w-full md:w-[48%]">
-                    <h2 className="font-semibold pb-3">Hexadecimal</h2>
-                    <input type="number" name="hexaResult" id="hexaResult" className="w-full p-4 bg-slate-100 border border-gray-500 rounded-lg focus:outline-none" value={hexaValue} readOnly />
-                </div>
+                <ImutableNumberInput title="Binary" name="binaryResult" id="binaryResult" value={binaryValue} />
+                <ImutableNumberInput title="Octal" name="octalResult" id="octalResult" value={octalValue} />
+                <ImutableNumberInput title="Hexadecimal" name="hexaResult" id="hexaResult" value={hexaValue} />
             </div>
         </div>
     )
